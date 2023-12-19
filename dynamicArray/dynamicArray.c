ce963a015ba89219ce71c4e92b7ad8cc0e668ec9
mrich888 <850338256@qq.com>
2023-12-19 21:02:46 +0800

    [修改] 完成动态数组初始化函数接口

--- dynamicArray/dynamicArray.c
+++ dynamicArray/dynamicArray.c
@@ -1,21 +1,48 @@
-#ifndef __DYNAMIC_ARRAY_H_
-#define __DYNAMIC_ARRAY_H_
+#include "dynamicArray.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/* 避免头文件重复包含 */
-
-typedef int ELEMENTTYPE;
-
-typedef struct dynamaicArray
+/* 状态码 */
+enum STATUS_CODE
 {
-    ELEMENTTYPE *data;  /* 数组的空间 */
-    int len;            /* 数组的大小 */
-    int capacity;       /* 数组的容量 */
-}dynamaicArray;
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+};
 
-/* API: application program interface. */
+#define DEFAULT_SIZE 10
 
 /* 动态数组的初始化 */
-int dynamaicArrayInit(dynamaicArray *pArray, int capacity);
+int dynamaicArrayInit(dynamaicArray *pArray, int capacity)
+{
+    if(pArray == NULL)
+    {
+        return NULL_PTR;
+    }
+
+    /* 避免传入非法值 */
+    if(capacity < 0)
+    {
+        capacity = DEFAULT_SIZE;
+    }
+
+    /* 分配空间 */
+    pArray->data = (ELEMENTTYPE *)malloc(sizeof(ELEMENTTYPE) * capacity); 
+    if(pArray->data == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+
+    /* 清楚脏数据 */
+    memset(pArray->data, 0, sizeof(ELEMENTTYPE) * capacity);
+
+    /* 初始化动态数组的参数属性 */
+    pArray->len = 0;
+    pArray->capacity = capacity;
+
+    return ON_SUCCESS;
+}
 
 /* 动态数组插入数据（默认查到数组的末尾）*/
 int dynamaicArrayInsertData(dynamaicArray *pArray, ELEMENTTYPE val);
@@ -43,5 +70,3 @@ int dynamaicArrayGetSize(dynamaicArray *pArray, int *pSize);
 
 /* 获取数组的容量 */
 int dynamaicArrayGetCapacity(dynamaicArray *pArray, int *Capacity);
-
-#endif //__DYNAMIC_ARRAY_H_
