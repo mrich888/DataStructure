9ccce7db6420cba33a77ff18a0ed7fd23a4c156a
mrich888 <850338256@qq.com>
2023-12-20 11:15:45 +0800

    [修改完善插入元素的APIP接口I

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -32,32 +32,81 @@ int LinkListInit(LinkList ** pList)
     /* 清空脏数据 */
     memset(list->head, 0,sizeof(LinkNode) * 1);
 
+    /* 好像有问题 */
     list->head->data = 0;
     list->head->next = NULL;
     /* 链表的长度为0 */
     list->len = 0;
 
-    /* 二级指针 */
-    *pList =list;
+    /* 二级指针*/
+    *pList = list;
 
 }
 
 /* 链表头插 */
 int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val)
 {
-
+    return LinkListAppointPosInsert(pList, 0, val);
 }
 
 /* 链表尾插 */
 int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val)
 {
-
+    return LinkListAppointPosInsert(pList, pList->len, val);
 }
 
 /* 链表指定位置插 */
 int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val)
 {
+    int ret;
+    if(pList == NULL)
+    {
+        return NULL_PTR;
+    }
 
+    if(pos < 0 || pos > pList->len)
+    {
+        return INVALID_ACCESS;
+    }
+
+    /* 封装一个结点 */
+    LinkNode * newNode = (LinkList *)malloc(sizeof(LinkNode) * 1);
+    if(newNode == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+
+    /* 清楚脏数据 */
+    memset(newNode, 0, sizeof(LinkNode) * 1);
+#if 0
+    newNode ->data = 0;
+    newNode-> 
+#endif
+    /* 赋值 */
+    newNode->data = val;
+
+#if 1
+    /* 头结点和虚拟头结点不一样 */
+    /* 从虚拟头结点开始遍历 */
+    LinkNode * travelNode = pList->head; /* pList是连接结点的线 */
+#else
+    LinkNode * travelNode = pList->head->next;
+#endif
+    while (pos)
+    {
+        travelNode = travelNode->next;/* 找到想要插入数据的位置 */
+        pos--;
+    }
+
+    /* 修改结点指向 */
+    /* 先断再连，先后再前 */
+    newNode->next = travelNode->next; 
+    travelNode->next = newNode;
+
+    /* 更新链表长度 */
+    (pList->len)++;
+
+    return ret;
 }
 
 /* 链表头删 */
@@ -87,7 +136,17 @@ int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE val)
 /* 获取链表的长度 */
 int LinkListGetLength(LinkList * pList, int *pSize)
 {
+    int ret = 0;
+    if(pList == NULL)
+    {
+        return NULL_PTR;
+    }
+    if(pSize)
+    {
+        *pSize = pList->len;
+    }
 
+    return ret;
 }
 
 /* 链表的销毁 */
